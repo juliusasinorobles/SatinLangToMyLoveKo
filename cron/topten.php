3614dec28b580b3fa0f4865d33abeac6f86484af
Charmaine Pineda <cpineda@yondu.com>
2014-07-13 15:29:10 +0800

    Signed-off-by: Charmaine Pineda <cpineda@yondu.com>

---
+++ cron/topten.php
@@ -0,0 +1,52 @@
+<?php
+	
+	$host = 'localhost';
+	$user = 'root';
+	$pass = '';
+	$database = 'underdogidols';
+	
+	$rank = 0;
+	$round_month = intval(date('m')) . ',' . date('Y'); 
+	
+	echo $round_month;
+	
+	$connection = mysql_connect($host, $user, $pass);
+	
+	if(!$connection):
+		die('Could not connect: ' . mysql_error());
+	endif;
+	
+	mysql_select_db($database) or die(mysql_error());
+	
+	$select_top = mysql_query("
+				SELECT 
+					video.contestant_id as contestant_id,
+					video.id as video_id,
+					video.round_month as round_month,
+					COUNT(*) AS votes
+				
+				FROM video 
+				INNER JOIN vote
+				ON video.id = vote.video_id
+				WHERE video.round_month = '$round_month'
+				AND video.round = 1
+				GROUP BY video.id
+				ORDER BY votes asc
+				LIMIT 10;	
+				");
+	
+	if($select_top) :	
+		while($row = mysql_fetch_array($select_top)):
+			$rank ++;
+			$sql = "INSERT INTO top_ten 
+					(contestant_id, video_id, round_month, ranking, votes)
+					VALUES
+					('".$row['contestant_id']."', '".$row['video_id']."', 
+						'".$round_month."', '".$rank."', '".$row['votes']."')";
+						
+			mysql_query($sql) or die(mysql_error());			
+		endwhile;
+	endif;
+	
+	mysql_close($connection);
+?>
